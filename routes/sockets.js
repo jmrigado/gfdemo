f0f9d69fe18618b227f1653d270aa396fa17d7d0
Matt McLaughlin <mclaughlin.matt@gmail.com>
2015-12-11 13:14:03 -0800

    Displays PH sensor data in 1 second intervals

--- routes/sockets.js
+++ routes/sockets.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const fs = require('fs')
+const rl = require('readline')
+
 module.exports = io => {
   io.on('connection', function (socket) {
     console.log('A user connected');
@@ -12,21 +15,40 @@ module.exports = io => {
         point: Math.floor(Math.random() * 100) + 1
       })
 
-      socket.emit('data::ph', {
-        ts: now,
-        point: Math.floor(Math.random() * 100) + 1
-      })
 
-      socket.emit('data::temp', {
-        ts: now,
-        point: Math.floor(Math.random() * 100) + 1
-      })
-
-      socket.emit('data::mv', {
-        ts: now,
-        point: Math.floor(Math.random() * 100) + 1
-      })
     }, 50)
+
+    fs.readFile('PHSensor-1hr.txt', 'utf8', (err, data) => {
+      let lines = data.split('\n')
+
+      let readLine = function readLine(lines) {
+        if (!lines.length) return;
+        let line = lines.shift();
+
+        setTimeout(() => {
+          let parts = line.replace(/\+/g, '').split(',')
+
+          socket.emit('data::ph', {
+            ts: parts[0],
+            point: parseFloat(parts[1])
+          })
+
+          socket.emit('data::temp', {
+            ts: parts[0],
+            point: parseFloat(parts[3])
+          })
+
+          socket.emit('data::mv', {
+            ts: parts[0],
+            point: parseFloat(parts[2])
+          })
+
+          return readLine(lines)
+        }, 1000)
+      }
+
+      readLine(lines)
+    })
 
     setInterval(() => {
       socket.emit('data::gpm::hourly', {
