764ac16eab2cd42c279120b5ddb6a085e924224a
jmrivetry <james@rivetry.com>
2016-01-04 16:23:29 -0800

    fixed line chart issues with Google charts

--- public/javascripts/linechart.js
+++ public/javascripts/linechart.js
@@ -1,41 +1,81 @@
 (function (global) {
   // Initial Data
+  var timeFormat = 'MM/DD/YYYY HH:mm';
   var ts = new Date();
   ts = new Date(ts.setHours(ts.getHours() - 1));
-  var chartData = global.initLineGraphData.map(function (value) {
+
+  var dataSet = {
+    labels: [],
+    datasets: [
+      {
+        label: "Flow",
+        backgroundColor: "rgba(33,150,243,0.2)",
+        borderColor: "rgba(33,150,243,0.8)",
+        pointBackgroundColor: "rgba(33,150,243,0.8)",
+        pointBorderColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: []
+      }
+    ]
+  };
+
+  for (value of global.initLineGraphData){
     ts = new Date(ts.setSeconds(ts.getSeconds() + 30));
-    return [ts, value[1]];
-  });
+    dataSet.labels.push(ts);
+    dataSet.datasets[0].data.push(value[1]);
+  }
 
-  google.load('visualization', '1', {'packages':['line']});
-  google.setOnLoadCallback(function () {
-    drawChart(chartData);
-    startListening();
-  });
+  drawChart(dataSet);
+  startListening();
 
-  function drawChart(points) {
-      // Create the data table.
-      var cData = [['Time', 'GPM']].concat(points);
-      var data = google.visualization.arrayToDataTable(cData);
+  function drawChart(data) {
+    // Create the data table.
+    // Set chart options
 
-      // Set chart options
-      var options = {
-          //title: 'Inlet Flow',
-          hAxis: {title: 'Time',  titleTextStyle: {color: '#333'}},
-          vAxis: {title: 'GPM',  minValue: 0},
-          height: 300
-      };
+    var config = {
+      type: 'line',
+      data: dataSet,
+      options: {
+        datasetFill : true,
+        responsive: true,
+        scaleShowVerticalLines: false,
+        pointDotRadius : 2,
+        scales: {
+          xAxes: [{
+            type: "time",
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Time'
+            }
+          }, ],
+          yAxes: [{
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'GPM'
+            }
+          }]
+        }
+      }
+    }
 
-      //create and draw the chart from DIV
-      var chart = new google.visualization.AreaChart(document.getElementById('lineChart'));
-      chart.draw(data, options);
+    //create and draw the chart from DIV
+    var ctx = document.getElementById("lineChart").getContext("2d");
+    var lineChart = new Chart(ctx, config);
   }
 
   function startListening() {
     window.app.onSocketEvent('data::gpm::hourly', function (data) {
-      chartData.push([new Date(), data.point]);
-      if (chartData.length > 108) chartData.shift();
-      drawChart(chartData);
+      dataSet.labels.push(new Date());
+      dataSet.datasets[0].data.push(data.point);
+
+      if (dataSet.length > 108) {
+        dataSet.labels.shift();
+        dataSet.datasets[0].data.shift();
+      }
+      drawChart(dataSet);
     });
   }
 
--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -2,11 +2,11 @@ $('.button').click(function(){
     $(this).addClass('active');
     setTimeout(function(){
         $('.popup').fadeOut();
-    }, 500)
+    }, 250)
 })
 
 $('#info').click(function(){
-    $('.popup').removeClass('active').delay(500).fadeIn();
+    $('.popup').removeClass('active').delay(250).fadeIn();
     $('.button').removeClass('active');
     $('.ok-reply').removeClass('active');
 })
